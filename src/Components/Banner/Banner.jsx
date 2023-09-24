ed8715be2ec3214d7e88dc556adb1ca38f66c283
Tahsin0909 <tajisan789@gmail.com>
2023-09-24 23:57:06 +0600

    Update Navbar and Banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,13 +4,13 @@ const Banner = () => {
     return (
         <div className="">
                 <div className="absolute top-0 ">
-                    <img className="w-screen h-screen opacity-50" src={'https://i.ibb.co/8YNchrV/banner.jpg'} alt="" />
+                    <img className="w-screen lg:h-screen h-[300px] opacity-50" src={'https://i.ibb.co/8YNchrV/banner.jpg'} alt="" />
                 </div>
-                <div className=" flex flex-col justify-center items-center gap-8 relative top-32">
-                    <p className="bg-gradient-to-r from-blue-600 to-red-600 bg-clip-text text-transparent  font-bold text-4xl">We Grow By Helping People In Need</p>
-                    <div className="flex gap-4">
-                        <input type="text" placeholder="category" className="input input-bordered input-info w-96 max-w-xs" />
-                        <button className="btn btn-accent">Search</button>
+                <div className=" flex flex-col justify-center items-center gap-8 relative lg:top-32">
+                    <p className="bg-gradient-to-r from-blue-600 to-red-600 bg-clip-text text-transparent text-center font-bold text-xl md:text-2xl lg:text-4xl">We Grow By Helping People In Need</p>
+                    <div className="flex gap-2 lg:gap-4">
+                        <input type="text" placeholder="category" className="input input-bordered input-info lg:w-96 md:w-64 w-40 h-10  lg:h-14 " />
+                        <button className="bg-blue-500 p-2 rounded-lg text-white h-10 lg:h-14 lg:p-4 hover:bg-blue-600">Search</button>
                     </div>
                 </div>
 
--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -2,38 +2,62 @@ import { NavLink } from "react-router-dom";
 
 
 const NavBar = () => {
+
+    const NavElement = () => {
+        return (
+            <>
+                <li>
+                    <NavLink
+                        to={`/`}
+                        className={({ isActive, isPending }) => isActive ? "text-red-500 underline" : isPending ? "pending" : ""}
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to={'/donation'}
+                        className={({ isActive, isPending }) => isActive ? "text-red-500 underline" : isPending ? "pending" : ""}
+                    >
+                        Donation
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to={`/statistics`}
+                        className={({ isActive, isPending }) => isActive ? "text-red-500 underline" : isPending ? "pending" : ""}
+                    >
+                        Statistics
+                    </NavLink>
+                </li>
+            </>
+        )
+    }
     return (
-        <div className="flex justify-between items-center mx-32 relative z-50 font-bold">
-            <div>
-                <img src={'https://i.ibb.co/N3Rxhs8/Logo.png'} alt="" />
-            </div>
-            <div >
-                <ul className="flex gap-10">
-                    <li>
-                        <NavLink
-                            to={`/`}
-                            className={({ isActive, isPending }) => isActive ? "text-blue-500 underline" : isPending ? "pending" : ""}
-                        >
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to={'/donation'}
-                            className={({ isActive, isPending }) => isActive ? "text-blue-500 underline" : isPending ? "pending" : ""}
-                        >
-                            Donation
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to={`/statistics`}
-                            className={({ isActive, isPending }) => isActive ? "text-blue-500 underline" : isPending ? "pending" : ""}
-                        >
-                            Statistics
-                        </NavLink>
-                    </li>
-                </ul>
+        <div>
+            <div className="flex lg:flex-row flex-row-reverse justify-between items-center py-4 mx-4 lg:mx-32 relative z-50 font-bold  ">
+                <div>
+                    <img className="w-28 md:w-36 lg:w-44" src={'https://i.ibb.co/N3Rxhs8/Logo.png'} alt="" />
+                </div>
+                <div >
+                    <ul className="lg:flex hidden gap-10 ">
+                        {
+                            <NavElement></NavElement>
+                        }
+                    </ul>
+                    <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {
+                                <NavElement></NavElement>
+                            }
+                        </ul>
+                    </div>
+                </div>
+                </div>
             </div>
         </div>
     );
